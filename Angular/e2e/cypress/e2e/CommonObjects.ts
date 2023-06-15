b8b794bd2f70a224ff529b0bc708c77161292b18
Yasin Allie <alliey@legal.regn.net>
2023-06-15 12:29:20 +0200

    added commobnObjects.ts

---
+++ Angular/e2e/cypress/e2e/CommonObjects.ts
@@ -0,0 +1,37 @@
+export class CommonObjects {
+  DashboardButton = (): string => {
+    return "//a[text()='Dashboard']";
+  };
+  HeroesButton = (): string => {
+    return "//a[text()='Heroes']";
+  };
+  SelectFirstHero = (): string => {
+    return "//div[@class='heroes-menu']/a[1]";
+  };
+  HeroSearchInput = (): string => {
+    return "//input[@id='search-box']";
+  };
+  HeroSearchFirstItem = (): string => {
+    return "//*[@id='search-component']/ul/li/a";
+  };
+  ClearButton = (): string => {
+    return "//button[@class='clear']";
+  };
+  HeroNameInput = (): string => {
+    return "//input[@id='hero-name']";
+  };
+  GoBackButton = (): string => {
+    return "//button[text()='go back']";
+  };
+  SaveButton = (): string => {
+    return "//button[text()='save']";
+  };
+
+  ExecuteSearch() {
+    this.ClickButtonByXpath(this.HeroSearchFirstItem());
+  }
+
+  ClickButtonByXpath(ElementXpath: string) {
+    cy.xpath(ElementXpath).click();
+  }
+}
